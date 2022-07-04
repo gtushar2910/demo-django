380209cb5a92447c76763aa81c0e178af25caf8e
Prof. Tushar Gohil <goheltushar1401@gmail.com>
2022-07-04 14:24:08 +0530

    Tuto 7

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -9,7 +10,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-
+    
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - timedelta(days=1)
 
